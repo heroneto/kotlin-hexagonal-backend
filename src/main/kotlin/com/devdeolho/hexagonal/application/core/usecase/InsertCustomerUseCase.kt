514a494c7440692c5d015678dce18ac7ec51b2d5
Heron Eto <hhqe@hotmail.com>
2024-06-17 19:48:48 -0300

    feat: add caso de uso de criacao de cliente

---
+++ src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/InsertCustomerUseCase.kt
@@ -0,0 +1,19 @@
+package com.devdeolho.hexagonal.application.core.usecase
+
+import com.devdeolho.hexagonal.application.core.domain.Customer
+import com.devdeolho.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
+import com.devdeolho.hexagonal.application.ports.out.InsertCustomerOutputPort
+
+class InsertCustomerUseCase(
+    private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
+    private val insertCustomerOutputPort: InsertCustomerOutputPort
+) {
+
+    fun insert(customer: Customer, zipCode: String) {
+        customer.apply {
+            address = findAddressByZipCodeOutputPort.find(zipCode)
+        }.let {
+            insertCustomerOutputPort.insert(it)
+        }
+    }
+}
