89c836b5e1c2c29771bb54cfcfba5cf3282d331e
Heron Eto <hhqe@hotmail.com>
2024-06-18 18:23:32 -0300

    feat: cria adapter para inserir cliente

--- src/main/kotlin/com/devdeolho/hexagonal/adapters/out/repository/entity/AdressEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.devdeolho.hexagonal.adapters.out.repository.entity
-
-data class AdressEntity(
-    val street: String,
-    val city: String,
-    val state: String
-)
--- src/main/kotlin/com/devdeolho/hexagonal/adapters/out/repository/entity/CustomerEntity.kt
+++ src/main/kotlin/com/devdeolho/hexagonal/adapters/out/repository/entity/CustomerEntity.kt
@@ -1,12 +1,21 @@
 package com.devdeolho.hexagonal.adapters.out.repository.entity
 
+import com.devdeolho.hexagonal.application.core.domain.Customer
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.MongoId
 @Document(collection = "customers")
 data class CustomerEntity(
     @MongoId val id: String?,
     val name: String,
-    val address: AdressEntity,
+    val address: AddressEntity,
     val cpf: String,
     val isValidCpf: Boolean
-)
+) {
+    constructor(customer: Customer): this(
+        customer.id,
+        customer.name,
+        AddressEntity(customer.address!!), // Operador !! precisa de atenção pois caso passe nulo ocorrerá erro de NullPointer.
+        customer.cpf,
+        customer.isValidCpf
+    )
+}
