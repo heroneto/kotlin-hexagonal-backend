6cf015b3d7e0b3b077f34789340e39793207301f
Heron Eto <hhqe@hotmail.com>
2024-06-18 18:34:54 -0300

    feat: cria controller

--- src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/InsertCustomerUseCase.kt
+++ src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/InsertCustomerUseCase.kt
@@ -1,15 +1,16 @@
 package com.devdeolho.hexagonal.application.core.usecase
 
 import com.devdeolho.hexagonal.application.core.domain.Customer
+import com.devdeolho.hexagonal.application.ports.`in`.InsertCustomerInputPort
 import com.devdeolho.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
 import com.devdeolho.hexagonal.application.ports.out.InsertCustomerOutputPort
 
 class InsertCustomerUseCase(
     private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
     private val insertCustomerOutputPort: InsertCustomerOutputPort
-) {
+) : InsertCustomerInputPort {
 
-    fun insert(customer: Customer, zipCode: String) {
+    override fun insert(customer: Customer, zipCode: String) {
         customer.apply {
             address = findAddressByZipCodeOutputPort.find(zipCode)
         }.let {
